efa64cc6c8cdf00d575f51cdb266426605647f3c
Billy <s.pbilly@yahoo.co.uk>
2023-10-30 16:35:56 +0000

    fetch working

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
+const { error } = require("console");
+
 const fruitForm = document.querySelector("#inputSection form")
 fruitForm.addEventListener("submit", extractFruit);
 
 function extractFruit(e){
+    //prevents form from refreshing page
     e.preventDefault()
-    addFruit(e.target[0].value)
+    fetchFruitData(e.target[0].value)
     e.target[0].value = ""
 }
 
+function fetchFruitData(fruit){
+    fetch(`https://fruity-api.onrender.com/fruits/${fruit}`)
+        .then(processResponse)
+        // .then(resp => resp.json())
+        .then(data => addFruit(data)) 
+        .catch(err => console.log(err))
+}
+
+function processResponse(resp){
+    if(resp.ok){
+        return resp.json()
+    } else {
+        throw "Error: http status code = " + resp.status
+    }
+}
+
 const fruitList =document.querySelector("#fruitSection ul");
+const fruitNutrition = document.querySelector("#nutritionSection p")
+
+let calories = 0
+const fruitCal = {}
+
 function addFruit(fruit){
+    console.log(fruit, addFruit)
     if(!fruit){
         console.log("Invalid fruit")
     } else {
     const li = document.createElement("li")
-    li.textContent = fruit
+    li.addEventListener("click", removeFruit)
+    li.textContent = fruit['name']
     fruitList.appendChild(li)
-    }
+
+    fruitCal[fruit.name] = fruit.nutritions.calories
+    console.log(fruitCal)
+
+    calories += fruit.nutritions.calories;
+    fruitNutrition.textContent = calories
 }
+}
+
+function removeFruit(e){
+    console.log(e)
+    const fruitName = e.target.textContent
+    calories -= fruitCal[fruitName]
+    fruitNutrition.textContent = calories
+
+    delete fruitCal[fruitName]
+    console.log(fruitName)
+    e.target.remove()
+}
+
